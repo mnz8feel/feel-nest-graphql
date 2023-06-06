3713c8d0b3b1bafde06c7736369b885bb3966363
mnz8 <mnz8@foxmail.com>
2023-06-07 00:22:39 +0800

    feat: graphql typeorm postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PokemonModule } from './managers/pokemon/pokemon.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: 'schema.gql',
+        }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres-user',
+            password: 'postgres-password',
+            database: 'postgres-db',
+            entities: ['dist/**/*.entity.js'],
+            // synchronize: true,
+            logging: true,
+        }),
+        PokemonModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
